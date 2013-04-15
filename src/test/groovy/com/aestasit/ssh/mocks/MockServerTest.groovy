8fee303cc7315c1e9e97b876252f87c4b42169e7
Andrey Adamovich <andrey@aestasit.com>
2013-04-15 12:21:58 +0200

    added a test

--- src/test/groovy/com/aestasit/ssh/mocks/MockServerTest.groovy
+++ src/test/groovy/com/aestasit/ssh/mocks/MockServerTest.groovy
@@ -2,5 +2,13 @@ package com.aestasit.ssh.mocks
 
 import static org.junit.Assert.*
 
+import org.junit.Test
+
 class MockServerTest {
+
+  @Test
+  void testMockSshd() throws Exception {
+    MockSshServer.startSshd()
+    MockSshServer.stopSshd()
+  }
 }
