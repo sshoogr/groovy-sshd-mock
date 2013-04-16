89e0a50dbe1e06effc1fe868b69ac7a9558be1fb
Andrey Adamovich <andrey@aestasit.com>
2013-04-16 12:40:36 +0200

    removed printing

--- src/main/groovy/com/aestasit/ssh/mocks/MockCommand.groovy
+++ src/main/groovy/com/aestasit/ssh/mocks/MockCommand.groovy
@@ -43,7 +43,6 @@ class MockCommand implements org.apache.sshd.server.Command {
   }
 
   def void start(Environment env) throws IOException {
-    println "Executing $command"
     Closure commandCode
     if (command) {
       commandCode = MockSshServer.commands.find { command == it.key || command.matches(it.key) }?.value
@@ -51,7 +50,6 @@ class MockCommand implements org.apache.sshd.server.Command {
     if (commandCode) {
       commandCode(inp, out, err, callback, env)
     } else {
-      println "Unknown command: $command"
       err << "Unknown command: $command"
       callback.onExit(-1)
     }
